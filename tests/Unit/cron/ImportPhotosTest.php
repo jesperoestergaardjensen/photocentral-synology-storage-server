f0f0a25965fc4d80c9108859a7d7ddc4ce1c4ab7
Jesper Østergaard Jensen <jesper@onlinelife.dk>
2022-07-03 13:20:53 +0200

    Added more debug output

--- tests/Unit/cron/ImportPhotosTest.php
+++ tests/Unit/cron/ImportPhotosTest.php
@@ -131,7 +131,10 @@ class ImportPhotosTest extends TestCase
         $expected_files_removed = $file_system_diff_report->getRemovedLinuxFilesMap();
 
         $this->assertCount(0, $expected_files_added);
-        var_dump($expected_files_moved); die('STOP');
+        var_dump(file_get_contents(self::getDataFolder() . "/status_files/SynologyPhotoCollection-2b613e8d-dd2a-4f1c-85a5-6e708290c200-new.txt"));
+        var_dump(file_get_contents(self::getDataFolder() . "/status_files/SynologyPhotoCollection-2b613e8d-dd2a-4f1c-85a5-6e708290c200-old.txt"));
+        var_dump($expected_files_moved);
+        return true;
         $this->assertCount(1, $expected_files_moved);
         $this->assertCount(0, $expected_files_removed);
     }
